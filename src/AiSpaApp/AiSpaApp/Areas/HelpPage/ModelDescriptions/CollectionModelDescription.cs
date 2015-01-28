d5c6f9225e03b2dbf163f6305bc8f3f43e20d9d2
Andreas Hammar <andreas.hammar@jayway.com>
2015-01-28 12:45:56 +0100

    added SPA app

---
+++ src/AiSpaApp/AiSpaApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AiSpaApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ src/AiSpaApp/AiSpaApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AiSpaApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
